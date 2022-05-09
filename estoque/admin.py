074df8d553e3c03d4cc56870816af78e5fe90abb
vaguinaldo <vaguinaldo@outlook.com>
2022-05-09 16:06:21 -0300

    estoque admin

--- estoque/admin.py
+++ estoque/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Estoque
+from .models import Estoque, EstoqueItens
+
+
+class EstoqueItensInline(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
 
 
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInline,)
     list_display = (
         '__str__',
         'nf'

fdbffb3e85e4f933d7b1214b3e4c1773c05f1302
vaguinaldo <vaguinaldo@outlook.com>
2022-05-10 16:41:22 -0300

    criação dos templates

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     #path('produto/', include('produto.urls')),
     #path('estoque/', include('estoque.urls')),
 ]

9a8ddf6f2621b21a0d0a10929248d1d6e66bf12a
vaguinaldo <vaguinaldo@outlook.com>
2022-05-11 11:21:05 -0300

    criação lista de produtos

--- produto/views.py
+++ produto/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from .models import Produto
+
+
+def produto_list(request):
+    template_name = 'produto_list.html'
+    objects = Produto.objects.all()
+    context = {'object_list': objects}
+    return render(request, template_name, context)
+
+
 
--- projeto/urls.py
+++ projeto/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    #path('produto/', include('produto.urls')),
+    path('produto/', include('produto.urls')),
     #path('estoque/', include('estoque.urls')),
 ]
+
+

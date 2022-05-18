40107a58e7673d8a616ddbef93d6ebbb0e25a1bc
vaguinaldo <vaguinaldo@outlook.com>
2022-05-18 15:40:24 -0300

    Saida: estoque_saida_list.html

--- estoque/urls.py
+++ estoque/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('estoque/', v.estoque_entrada_list, name="estoque_entrada_list"),
     path('<int:pk>/', v.estoque_entrada_datail, name="estoque_entrada_detail"),
     path('add/', v.estoque_entrada_add, name="estoque_entrada_add"),
+    path('saida/', v.estoque_saida_list, name="estoque_saida_list"),
 
 ]

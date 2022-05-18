c1818b82a8efee3f1d96a454b79005f727d1ec91
vaguinaldo <vaguinaldo@outlook.com>
2022-05-18 11:50:47 -0300

    Ajax para atualizar o saldo de cada produto

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView
-
+from django.http import JsonResponse
 from .models import Produto
 from .forms import ProdutoForm
 
@@ -34,6 +34,11 @@ class ProdutoUpdate(UpdateView):
     template_name = 'produto_form.html'
     form_class = ProdutoForm
 
+
+def produto_json(request, pk):
+    produto = Produto.objects.filter(pk=pk)
+    data=[item.to_dict_json() for item in produto]
+    return JsonResponse({'data':data})
 
 
 
